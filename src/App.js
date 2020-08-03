a7e9c7a2ff3715c2ec03654250570bbf4eee162c
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-02 21:41:07 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div className="App">
+        <p>hello app.js</p>
+      </div>
+
+    ) // end return
+  } // end render
+} // end class
+
+export default App;
